e9974e0682ce57c24b1840d9f73001ac72d3be29
Jean Kiepura <jkiepura@k2geospatial.com>
2025-03-19 07:51:30 -0400

    [feature/JMAP8-1769_JMAP8-1360] - rename some interfaces/enums

--- public/jmap/server.d.ts
+++ public/jmap/server.d.ts
@@ -1,13 +1,13 @@
-// ALL_SERVER_SAAS_STATUS in all-enum.ts
-declare const enum JSERVER_STATUS {
+// ALL_SERVER_MICRO_SERVICE_STATUSES in all-enum.ts
+declare const enum JSERVER_MICRO_SERVICE_STATUSES {
   STARTING = "STARTING",
   UP = "UP",
   DOWN = "DOWN"
 }
 
-// ALL_SERVER_SAAS_STATUS in all-enum.ts
+// ALL_SERVER_IDENTITY_PROVIDER_TYPES in all-enum.ts
 declare const enum JSERVER_IDENTITY_PROVIDER_TYPES {
-  SSO = "sso",
+  JMAP_CLOUD = "jmapcloud.io", // reserved for future usage ("standard login" via REST API)
   AUTH0_SPA = "auth0-spa"
 }
 
@@ -15,7 +15,7 @@ declare interface JServerIdentityProviderById {
   [id: string]: JServerAnyIdentityProvider
 }
 
-declare interface JServerServiceById {
+declare interface JServerMicroServiceById {
   [id: string]: JServerService
 }
 
@@ -23,18 +23,18 @@ declare interface JServerInfo {
   identityProviderById: JServerIdentityProviderById
   standardLoginAvailable: boolean
   version: JServerVersion
-  serviceById?: JServerServiceById
+  microServiceById: JServerMicroServiceById
 }
 
 declare interface JServerService {
   id: string
   name: string
   version: string
-  status: JSERVER_STATUS
+  status: JSERVER_MICRO_SERVICE_STATUSES
   restBaseUrl: string
 }
 
-declare type JServerAnyIdentityProvider = JServerIdentityProviderAuth0Password | JServerIdentityProviderSso
+declare type JServerAnyIdentityProvider = JServerIdentityProviderAuth0Spa | JServerIdentityProviderJMapCloudNative
 
 declare interface JServerIdentityProviderBase {
   id: string
@@ -42,17 +42,16 @@ declare interface JServerIdentityProviderBase {
   type: JSERVER_IDENTITY_PROVIDER_TYPES
 }
 
-declare interface JServerIdentityProviderAuth0Password extends JServerIdentityProviderBase {
+declare interface JServerIdentityProviderAuth0Spa extends JServerIdentityProviderBase {
   type: JSERVER_IDENTITY_PROVIDER_TYPES.AUTH0_SPA
   domain: string
   clientId: string
   realm: string
+  logoutRedirectUrl: string
 }
 
-declare interface JServerIdentityProviderSso extends JServerIdentityProviderBase {
-  type: JSERVER_IDENTITY_PROVIDER_TYPES.SSO
-  imageData: string
-  loginUrl: string
+declare interface JServerIdentityProviderJMapCloudNative extends JServerIdentityProviderBase {
+  type: JSERVER_IDENTITY_PROVIDER_TYPES.JMAP_CLOUD
 }
 
 declare interface JServerVersion {
